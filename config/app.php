3cb5dc05784a99b4e7884845faafdb529357fdb5
Leo1nardo1 <leonardo.carvalho.ferreira@protonmail.com>
2024-01-23 23:28:47 -0300

    mudanças

--- config/app.php
+++ config/app.php
@@ -2,10 +2,10 @@
 
 session_start();
 
-  define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASSWORD', "password");
-  define('DB_DATABASE', 'fundodikintal');
+  define('DB_HOST', '');
+  define('DB_USER', '');
+  define('DB_PASSWORD', '');
+  define('DB_DATABASE', '');
 
 
   define('SITE_URL', 'http://localhost/aviaoprojeto');
